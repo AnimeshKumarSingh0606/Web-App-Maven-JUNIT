686de4c39747340dca677bae7bd016dfff7359d8
Animesh Kumar Singh <animesh.2001singh@gmail.com>
2021-02-21 11:55:43 +0530

    simple web app build using maven and JUNIT

---
+++ src/main/java/com/animesh/Simple.java
@@ -0,0 +1,15 @@
+package com.animesh;
+
+public class Simple{
+  public int printNumber(){
+    return 8;
+  }
+  
+  public int addTwoNumbers(int x, int y){
+	  return x+y;
+  }
+  
+  public int subtractTwoNumbers(int x, int y){
+	  return x-y;
+  }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2><font face="verdana" color="green">Welcome TO AdaptNext</font></h2>
+<i><font face="verdana" color="green">Version 101</font></i>
+</body>
+</html>
